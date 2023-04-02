92f303d8392aa4a9026e6f68195c5f0a239bb4bd
Raredombo <42446387+Raredombo@users.noreply.github.com>
2023-04-03 01:26:24 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-$(function () {
-    count = 0;
-    wordsArray = ["used", "abandoned", "extrovert", "introvert", "nostalgia", "2010", "mike", "suicide", "music", "the greatest", "creativity", "alleen op de wereld"];
-    setInterval(function () {
-      count++;
-      $("#word").fadeOut(400, function () {
-        $(this).text(wordsArray[count % wordsArray.length]).fadeIn(400);
-      });
-    }, 2000);
-  });
+
